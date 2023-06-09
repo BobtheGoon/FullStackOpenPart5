9596613dee6a860dd74e1a715db5cc9514731943
Bob <teemu9598@gmail.com>
2023-06-09 06:58:37 +0300

    Display logged in user

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.username} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
