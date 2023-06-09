6c4d91cf8476fb776832b6e2f86b69d0c65411e2
Bob <teemu9598@gmail.com>
2023-06-09 07:36:02 +0300

    Add logout functionality

--- src/App.js
+++ src/App.js
@@ -42,12 +42,22 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+    console.log('logged out')
+  }
+
   if (user === null) return LoginForm({username, setUserName, password, setPassword, handleLogin})
 
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in</p>
+      <div>
+        {user.username} logged in
+        <button onClick={handleLogout}>Log out</button>
+      </div>
+      <p></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
