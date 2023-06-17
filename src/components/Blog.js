94f328db1d8ae087e9aaac23e513087b54c7af53
Bob <teemu9598@gmail.com>
2023-06-17 23:23:37 +0300

    Add hacky way to display creator of blog listing, via useEffect

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -11,22 +11,31 @@ const Blog = ({blog}) => {
     <div>
       {!showAll &&
       <div className='blog'>
-        <h3>{blog.title}, {blog.author}</h3>
+        <h3>{blog.title}</h3>
+        <p>{blog.author}</p>
+
         <button onClick={toggleShowAll}>More info</button>
       </div>
       }
 
-      {showAll &&
+      {showAll && (
         <div className='blog'>
-          <h3>{blog.title}, {blog.author}</h3>
+          <h3>{blog.title}</h3>
+          <p>{blog.author}</p>
           <p>{blog.url}</p>
+          
           <div className='blog_likes'>
-            <p>Likes {blog.likes}</p>
+            {blog.likes === undefined &&
+            <p>Be the first to like this post! {blog.likes}</p>
+            }
             <button onClick={() => console.log('liked')}>Like</button>
           </div>
+
+          <p>{blog.user.username}</p>
+
           <button onClick={toggleShowAll}>Show less</button>
         </div>
-      }
+      )}
     </div>
   )
   }
