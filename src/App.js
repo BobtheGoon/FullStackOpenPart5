fd5bad46e177c6512cbd990b08f17d9ec68f3945
Bob <teemu9598@gmail.com>
2023-06-17 22:22:14 +0300

    Add button to show more blog info

--- src/App.js
+++ src/App.js
@@ -104,7 +104,6 @@ const App = () => {
         <Togglable buttonLabel='Add blog'>
           <BlogForm addBlog={addBlog}/>
         </Togglable>
-        <p></p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll)
+  }
+
+  return (
+    <div>
+      {!showAll &&
+      <div className='blog'>
+        <h3>{blog.title}</h3>
+        <button onClick={toggleShowAll}>More info</button>
+      </div>
+      }
+
+      {showAll &&
+        <div className='blog'>
+          <h3>{blog.title}</h3>
+          <p>{blog.url}</p>
+          <div className='blog_likes'>
+            <p>Likes {blog.likes}</p>
+            <button onClick={() => console.log('liked')}>Like</button>
+          </div>
+          <p>{blog.author}</p>
+          <button onClick={toggleShowAll}>Show less</button>
+        </div>
+      }
+    </div>
+  )
+  }
 
 export default Blog
