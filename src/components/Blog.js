60e527b5f7cad83aa79eff72d8958d4fdbeaf678
Bob <teemu9598@gmail.com>
2023-06-17 22:31:00 +0300

    Display blog author along  with title

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -11,20 +11,19 @@ const Blog = ({blog}) => {
     <div>
       {!showAll &&
       <div className='blog'>
-        <h3>{blog.title}</h3>
+        <h3>{blog.title}, {blog.author}</h3>
         <button onClick={toggleShowAll}>More info</button>
       </div>
       }
 
       {showAll &&
         <div className='blog'>
-          <h3>{blog.title}</h3>
+          <h3>{blog.title}, {blog.author}</h3>
           <p>{blog.url}</p>
           <div className='blog_likes'>
             <p>Likes {blog.likes}</p>
             <button onClick={() => console.log('liked')}>Like</button>
           </div>
-          <p>{blog.author}</p>
           <button onClick={toggleShowAll}>Show less</button>
         </div>
       }
