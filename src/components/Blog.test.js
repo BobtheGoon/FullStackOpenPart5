b511d5949933c815452e892468bf96a781101e4d
Bob <teemu9598@gmail.com>
2023-08-08 19:23:54 +0300

    Test that more content is shown when show more is clicked

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -12,6 +12,7 @@ describe('<Blog/>', () => {
     title: 'Testing blog',
     author: 'Jest testing',
     url: 'JestTest.com',
+    likes: 0,
     user: {
       username: 'Jest',
       name: 'Jest Tester',
@@ -28,5 +29,16 @@ describe('<Blog/>', () => {
     screen.getByText('Testing blog')
   })
 
+  test('likes and url are displayed when show more is clicked', async () => {
+    const mockHandler = jest.fn()
+
+    const user = userEvent.setup()
+    const button = screen.getByText('Show more')
+    await user.click(button)
+
+    screen.getByText('Jest')
+    screen.getByText('JestTest.com')
+    screen.getByText('Be the first to like this post!')    
+  })
 })
 
