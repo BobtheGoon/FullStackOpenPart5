4cfdaa24afb488700f14f816c57c58953a74a287
Bob <teemu9598@gmail.com>
2023-08-09 19:18:10 +0300

    Add test for like button

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -6,8 +6,6 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog/>', () => {
-  let container
-
   const blog = {
     title: 'Testing blog',
     author: 'Jest testing',
@@ -19,18 +17,14 @@ describe('<Blog/>', () => {
     }
   }
 
-  beforeEach(() => {
-    container = render(
-      <Blog blog={blog}></Blog>
-    )
-  })
-
   test('renders blog title', () => {
+    render(<Blog blog={blog}></Blog>)
     screen.getByText('Testing blog')
   })
 
-  test('likes and url are displayed when show more is clicked', async () => {
+  test('likes and url are displayed when show more is clicked', async() => {
     const mockHandler = jest.fn()
+    render(<Blog blog={blog}></Blog>)
 
     const user = userEvent.setup()
     const button = screen.getByText('Show more')
@@ -39,6 +33,21 @@ describe('<Blog/>', () => {
     screen.getByText('Jest')
     screen.getByText('JestTest.com')
     screen.getByText('Be the first to like this post!')    
+  })
+
+  test('like button eventhandler is called twice when button is clicked twice', async() => {
+    const mockHandler = jest.fn()
+    render(<Blog blog={blog} addLike={mockHandler}></Blog>)
+
+    const user = userEvent.setup()
+    const show_button = screen.getByText('Show more')
+    await user.click(show_button)
+
+    const like_button = screen.getByText('Like')
+    await user.click(like_button)
+    await user.click(like_button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
 
